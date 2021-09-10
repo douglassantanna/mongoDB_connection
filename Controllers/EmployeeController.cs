9902e2749233d7434a7dd0d7b04d919e1a8b6810
douglassantanna <douglbb1@gmail.com>
2021-09-10 16:56:48 -0300

    first commit

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace wcaApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EmployeeController : ControllerBase
+    {
+        private IMongoCollection<dynamic> _employees;
+        public EmployeeController(IMongoClient client)
+        {
+            var database = client.GetDatabase("employeeDB");
+            _employees = database.GetCollection<dynamic>("employee");
+        }
+
+        [HttpGet]
+        public ActionResult<dynamic> Get(string name)
+        {
+            if(name is null)
+                return  _employees.Find<dynamic>(new BsonDocument()).Limit(10).ToList();
+
+            var filtrar = Builders<dynamic>.Filter.Eq("name", name);
+            return _employees.Find(filtrar).ToList();
+        }
+    }
+}
+
