8dc8bab7cb4939a53e9cae541cdd4e55f1b16e28
douglassantanna <douglbb1@gmail.com>
2021-09-14 16:25:24 -0300

    controller changes

--- Controllers/EmployeeController.cs
+++ Controllers/EmployeeController.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
+﻿using System.Linq;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 using MongoDB.Bson;
 using MongoDB.Driver;
 
